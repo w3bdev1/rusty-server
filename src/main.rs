112417e6f243df5d98cd2de4603876416ea7bbc3
w3bdev <w3bdev@protonmail.com>
2022-02-08 15:17:30 +0530

    Multithread: Slow request simulation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -18,8 +20,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
