0fe2c3b1dbcba292fa87d354bad3ffa242036c66
w3bdev <w3bdev@protonmail.com>
2022-02-08 12:00:17 +0530

    Reading request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use std::net::TcpListener;
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("{}", String::from_utf8_lossy(&buffer[..]));
 }
