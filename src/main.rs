c93a95471f6278cbe711c42579b1f43ba27ae722
w3bdev <w3bdev@protonmail.com>
2022-02-08 15:35:45 +0530

    Multithread: Thread for each request (Not ideal)

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,10 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
